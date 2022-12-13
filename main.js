e9ad704636433afca2414251622ac66dc1f93d24
jousef1 <yousefel560@gmail.com>
2022-12-13 21:08:45 +0800

    vedio

---
+++ main.js
@@ -0,0 +1,14 @@
+// vedio pop-up
+
+// const layer = document.getElementsByClassName("layer")[0];
+// const vedio_player = document.getElementsByClassName("vedio_player")[0];
+// layer.addEventListener("click",()=>{console.log("llll");})
+
+// ./vedio pop-up
+
+// layer.addEventListener("click", myPopFunction);
+
+// function myPopFunction() {
+  // console.log(layer);
+// }
+
